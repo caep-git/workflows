fe4efecda6efa76c1d8006068e4464a552f0c406
Carlota Pearl <carlota.pearl@gmail.com>
2015-02-19 22:29:10 -0500

    Added Watch task

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -75,9 +75,9 @@ var $, fill;
 
 $ = require('jquery');
 
-(fill = function(item) {})('The most creative minds in Art');
-
-$('.tagline').append("" + item);
+(fill = function(item) {
+  return $('.tagline').append("" + item);
+})('The most creative minds in Art');
 
 fill;
 
